ef4ae3e00764a4ff296ac24f769d73f281ed9e59
stuff-new <RobertStuff>
2025-07-02 21:42:39 +0800

    id

--- truth-network-frontend/src/constants/constant-devnet.js
+++ truth-network-frontend/src/constants/constant-devnet.js
@@ -4,7 +4,7 @@ import { PublicKey } from "@solana/web3.js";
 export const FEE_RECEIVER = new PublicKey("CQaZgx5jqQrz7c8shCG3vJLiiPGPrawSGhvkgXtGyxL");
 
 // Program ID
-export const PROGRAM_ID = new PublicKey("9yAdGEsDDrgj73m1Q586dcN4tZqMnLMgnC86WAqsnXEs");
+export const PROGRAM_ID = new PublicKey("9DuZCtKqhQQr3qKwCm42NZgndSNHwLi3vt6edgV9VAoE");
 
 export const DEFAULT_RPC_URL = localStorage.getItem("solana_rpc_url") || "https://api.devnet.solana.com";
 
